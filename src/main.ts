b21a3e487989187cf701f0f16c257dee53199325
hutao <1273698633@qq.com>
2024-06-03 11:36:07 +0800

    设置跨域代理，引入element plus和axios

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
+app.config.globalProperties.$http = axios.create({
+    baseURL: '/api'
+})
+app.config.globalProperties.$test = axios.create({
+    baseURL: '/test'
+})
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,21 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server:{
+    host:'0.0.0.0',
+    port:'5173',
+		proxy: {
+			'/api':{
+				target:"http://localhost:50100",
+        changeOrigin:true, //支持跨域
+        rewrite:(path) => path.replace(/^\/api/, "")//重写路径,替换/api
+			},
+      '/test':{
+        target:"http://localhost:4523",
+        changeOrigin:true, //支持跨域
+        rewrite:(path) => path.replace(/^\/test/, "")//重写路径,替换/test
+      }
+		}
+	}
 })
